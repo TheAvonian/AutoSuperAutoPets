be1bdf70cd8cbf8602bb7943b0fecd952aea1ab1
Aven <29378966+TheAvonian@users.noreply.github.com>
2022-03-08 02:53:59 -0500

    Adding UI Changes

--- Auto Super Auto Pets/Assets/Scripts/UI/UIController.cs
+++ Auto Super Auto Pets/Assets/Scripts/UI/UIController.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-[RequireComponent(typeof(UIDocument))]
+[RequireComponent( typeof( UIDocument ) )]
 public class UIController : MonoBehaviour
 {
     public GameManager GameManager;
@@ -19,20 +20,76 @@ public class UIController : MonoBehaviour
 
     void Start()
     {
+        _allSprites = Resources.LoadAll( "Pets", typeof( Sprite ) ).Cast< Sprite >().ToArray();
+
+        _teamOnePets = new PetCard[ 5 ];
+        _teamTwoPets = new PetCard[ 5 ];
+
+        _shopCards = new PetCard[ 7 ];
+
         _healthElement = _document.rootVisualElement.Q< Label >( "TeamHealth" );
         _coinsElement = _document.rootVisualElement.Q< Label >( "TeamCoins" );
         _turnsElement = _document.rootVisualElement.Q< Label >( "TeamTurns" );
         _winsElement = _document.rootVisualElement.Q< Label >( "TeamWins" );
-        _iterationsElement = _document.rootVisualElement.Q< Label >( "GameIterations" );
+
+        List< VisualElement > teamOneHolder = _document.rootVisualElement.Q< GroupBox >( "TeamOne" ).Children().ToList();
+        List< VisualElement > teamTwoHolder = _document.rootVisualElement.Q< GroupBox >( "TeamTwo" ).Children().ToList();
+
+        List< VisualElement > shopHolder = _document.rootVisualElement.Q< GroupBox >( "ShopPets" ).Children().ToList();
+
+        for ( int i = 0; i < 5; i++ )
+        {
+            _teamOnePets[ i ] = new PetCard
+            {
+                HealthElement = teamOneHolder[ i ].Q< Label >( "PetHealth" ),
+                DamageElement = teamOneHolder[ i ].Q< Label >( "PetDamage" ),
+                FoodElement = teamOneHolder[ i ].Q< GroupBox >( "PetFood" ),
+                ImageElement = teamOneHolder[ i ].Q< GroupBox >( "PetImage" ),
+            };
+
+            _teamTwoPets[ i ] = new PetCard
+            {
+                HealthElement = teamTwoHolder[ i ].Q< Label >( "PetHealth" ),
+                DamageElement = teamTwoHolder[ i ].Q< Label >( "PetDamage" ),
+                FoodElement = teamTwoHolder[ i ].Q< GroupBox >( "PetFood" ),
+                ImageElement = teamTwoHolder[ i ].Q< GroupBox >( "PetImage" ),
+            };
+
+            _shopCards[ i ] = new PetCard
+            {
+                HealthElement = shopHolder[ i ].Q< Label >( "PetHealth" ),
+                DamageElement = shopHolder[ i ].Q< Label >( "PetDamage" ),
+                FoodElement = shopHolder[ i ].Q< GroupBox >( "PetFood" ),
+                ImageElement = shopHolder[ i ].Q< GroupBox >( "PetImage" ),
+            };
+        }
+
+        _shopCards[ 5 ] = new PetCard
+        {
+            HealthElement = shopHolder[ 5 ].Q< Label >( "PetHealth" ),
+            DamageElement = shopHolder[ 5 ].Q< Label >( "PetDamage" ),
+            FoodElement = shopHolder[ 5 ].Q< GroupBox >( "PetFood" ),
+            ImageElement = shopHolder[ 5 ].Q< GroupBox >( "PetImage" ),
+        };
+
+        _shopCards[ 6 ] = new PetCard
+        {
+            HealthElement = shopHolder[ 6 ].Q< Label >( "PetHealth" ),
+            DamageElement = shopHolder[ 6 ].Q< Label >( "PetDamage" ),
+            FoodElement = shopHolder[ 6 ].Q< GroupBox >( "PetFood" ),
+            ImageElement = shopHolder[ 6 ].Q< GroupBox >( "PetImage" ),
+        };
     }
+
+    Sprite[] _allSprites;
+    PetCard[] _teamOnePets;
+    PetCard[] _teamTwoPets;
+    PetCard[] _shopCards;
 
     Label _healthElement;
     Label _coinsElement;
     Label _turnsElement;
     Label _winsElement;
-    Label _iterationsElement;
-    int _totalTurns;
-    int _lastTurn;
     void LateUpdate()
     {
         _healthElement.text = GameManager.TeamOne.Health.ToString();
@@ -40,12 +97,116 @@ public class UIController : MonoBehaviour
         _turnsElement.text = GameManager.TeamOne.Shop.Turn.ToString();
         _winsElement.text = GameManager.TeamOne.Wins.ToString();
 
-        if ( GameManager.TeamOne.Shop.Turn < _lastTurn )
+        LinkedListNode< PetData > oneNode = GameManager.State is GameManager.GameState.Battle or GameManager.GameState.BattleStart or GameManager.GameState.BattleEnd ? GameManager.BattleTeamOne?.Pets?.First : GameManager.TeamOne?.Pets?.First;
+        LinkedListNode< PetData > twoNode = GameManager.BattleTeamTwo?.Pets?.First;
+        ShopItem shopNode = GameManager.TeamOne.Shop?.Items?.Count != 0 ? GameManager.TeamOne.Shop?.Items?[ 0 ] : null;
+        for ( int i = 0; i < 5; i++ )
         {
-            _totalTurns++;
-            _iterationsElement.text = _totalTurns.ToString();
+
+            if ( oneNode != null )
+            {
+                CreatePetCard( _teamOnePets[ i ], oneNode.Value );
+                oneNode = oneNode.Next;
+            }
+
+            if ( twoNode != null )
+            {
+                CreatePetCard( _teamTwoPets[ i ], twoNode.Value );
+                twoNode = twoNode.Next;
+            }
+
+            if ( shopNode != null )
+            {
+                CreatePetCard( _shopCards[ i ], shopNode );
+                shopNode = GameManager.TeamOne.Shop.TryGetItem( i + 1 );
+            }
         }
 
-        _lastTurn = GameManager.TeamOne.Shop.Turn;
+        if ( GameManager.TeamOne.Shop?.Items != null )
+        {
+            if ( GameManager.TeamOne.Shop.Items.Count > 5 )
+            {
+                CreatePetCard( _shopCards[ 5 ], GameManager.TeamOne.Shop.Items[ 5 ] );
+            }
+
+            if ( GameManager.TeamOne.Shop.Items.Count > 6 )
+            {
+                CreatePetCard( _shopCards[ 6 ], GameManager.TeamOne.Shop.Items[ 6 ] );
+            }
+        }
+
     }
+
+    void CreatePetCard( PetCard petCard, PetData petItem )
+    {
+        petCard.ImageElement.style.backgroundImage = new StyleBackground( _allSprites.First( x => x.name.Equals( GetPetName( petItem ) ) ) );
+        petCard.DamageElement.text = petItem.Damage.ToString();
+        petCard.HealthElement.text = petItem.Health.ToString();
+        if ( petItem.Food != FoodData.Food.None )
+        {
+            petCard.FoodElement.style.opacity = 100f;
+            petCard.FoodElement.style.backgroundColor = _borderColor;
+            petCard.FoodElement.style.backgroundImage = new StyleBackground( _allSprites.First( x => x.name.Equals( petItem.Food.ToString() ) ) );
+        } else
+        {
+            petCard.FoodElement.style.opacity = 0f;
+        }
+
+        petCard.ImageElement.style.backgroundColor = _borderColor;
+    }
+
+    readonly StyleFloat _borderWidth = new( 0f );
+    readonly StyleColor _borderColor = new( new Color( 1, 1, 1, 0 ) );
+
+    readonly StyleFloat _frozenWidth = new( 2f );
+    readonly StyleColor _frozenColor = new( Color.cyan );
+    void CreatePetCard( PetCard petCard, ShopItem shopItem )
+    {
+        if ( shopItem.Pet != null )
+        {
+            CreatePetCard( petCard, shopItem.Pet );
+            petCard.DamageElement.parent.style.opacity = 100f;
+            petCard.HealthElement.parent.style.opacity = 100f;
+        } else if ( shopItem.Food != null )
+        {
+            petCard.ImageElement.style.backgroundImage = new StyleBackground( _allSprites.First( x => x.name.Equals( shopItem.Food.ToString() ) ) );
+            petCard.DamageElement.parent.style.opacity = 0;
+            petCard.HealthElement.parent.style.opacity = 0;
+        }
+
+        petCard.ImageElement.style.backgroundColor = shopItem.Frozen ? _frozenColor : _borderColor;
+
+        petCard.FoodElement.style.opacity = 0f;
+    }
+
+    static string GetPetName( PetData item )
+    {
+        return item != null ? ( (PetData.AllPets) item.PetID ).ToString() : null;
+    }
+
+    static string GetShopItemName( ShopItem item )
+    {
+        if ( item != null )
+        {
+            if ( item.Food != null )
+            {
+                return item.Food.ToString();
+            }
+
+            if ( item.Pet != null )
+            {
+                return ( (PetData.AllPets) item.Pet.PetID ).ToString();
+            }
+        }
+
+        return null;
+    }
+}
+
+class PetCard
+{
+    public VisualElement ImageElement;
+    public VisualElement FoodElement;
+    public Label DamageElement;
+    public Label HealthElement;
 }
